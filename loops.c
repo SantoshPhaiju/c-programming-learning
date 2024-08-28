2f1e5a503f367c07a7e033a646403007484a8cee
Santosh Phaiju <92672422+SantoshPhaiju@users.noreply.github.com>
2024-08-28 10:47:08 +0545

    loop hehe

--- loops.c
+++ loops.c
@@ -52,5 +52,16 @@ int main()
 
     printf("sum of first 10 numbers is %d\n", sum);
 
+
+    // Print all odd numbers from 1 to 100
+
+    for (int i = 1; i <= 100; i++ ) {
+        if (i % 2 != 0) {
+            printf("\n odd - %d", i);
+        }
+    }
+
+
+
     return 0;
 }
