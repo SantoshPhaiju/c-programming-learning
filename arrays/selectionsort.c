eecf62d812fac43e741100f11343a73122f3daaf
Santosh Phaiju <92672422+SantoshPhaiju@users.noreply.github.com>
2024-09-11 11:09:34 +0545

    sequential sort done

---
+++ arrays/selectionsort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int arr[] = {5, 3, 8, 6, 2, 7, 1, 4};
+
+    int minIndex = arr[0];
+    int temp;
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = i + 1; j < 8; j++)
+        {
+           if(arr[j] < arr[minIndex]){
+               minIndex = j;
+           }
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+
+    }
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("The value of arris %d \n", arr[i]);
+    }
+
+    return 0;
+}
