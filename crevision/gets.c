49451aa155829d0303c7b5ea109d3b84b6a96128
Santosh Phaiju <92672422+SantoshPhaiju@users.noreply.github.com>
2024-11-07 22:12:36 +0545

    add string input functionality using scanf

---
+++ crevision/gets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char st[10];
+    printf("Enter a string:- ");
+
+    printf("string => %s", st);
+
+    return 0;
+}
---
+++ crevision/stringusingscanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    char st[10];
+    printf("Enter a string:- ");
+    scanf("%s", st);
+    printf("string => %s", st);
+
+    return 0;
+}
