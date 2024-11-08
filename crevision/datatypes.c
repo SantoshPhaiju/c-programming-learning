586e3f79f4267fded8003b85c1c90a6319f8f186
Santosh Phaiju <92672422+SantoshPhaiju@users.noreply.github.com>
2024-11-08 22:15:05 +0545

    add basic C program demonstrating identifiers and keywords

---
+++ crevision/datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    // Identifiers & Keywords
+    // auto, double, int, struct, break, else, long, switch, case, enum, register, typedef, char, extern, return, union, const, float, short, while, continue, for, signed, void, default, goto, sizeof, volatile, do, if, static, unsigned
+
+    int a = 10;
+
+    return 0;
+}
