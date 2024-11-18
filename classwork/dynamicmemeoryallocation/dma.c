728c1f06b95265ae2881b1b10063379c399f6575
Santosh Phaiju <92672422+SantoshPhaiju@users.noreply.github.com>
2024-11-18 10:05:02 +0545

    add program to demonstrate dynamic memory allocation and sum calculation

---
+++ classwork/dynamicmemeoryallocation/dma.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, na, *ptr, sum = 0;
+
+    printf("Enter the number upto which you want to find the sum of:- ");
+    scanf("%d", &n);
+
+    ptr = (int *)malloc(n * sizeof(n));
+
+    if (ptr == NULL)
+    {
+        printf("Memory Allocation failed");
+        exit(1);
+    }
+
+    printf("Enter %d numbers: ", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", (ptr + i));
+        sum += *(ptr + i);
+    }
+
+    printf("The sum of your numbers is: %d", sum);
+
+    printf("\nHow many numbers do you want to add more:- ");
+    scanf("%d", &na);
+
+    ptr = (int *)realloc(ptr, na + 5 * sizeof(int));
+
+    for (int i = na; i < na + 5; i++)
+    {
+        scanf("%d", (ptr + i));
+        sum += *(ptr + i);
+    }
+    printf("The sum of your numbers is: %d", sum);
+
+    return 0;
+}
